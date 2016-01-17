1827f05d3490e6f00e262c302423f4c5f6801e80
Talrey <cycleman.atl@gmail.com>
2016-01-16 22:21:32 -0800

    Wrappable isn't dumb anymore
    It now defaults to TRUE, and uses the player's position instead of
    (d'oh) screen size.

--- Assets/Scripts/Wrappable.cs
+++ Assets/Scripts/Wrappable.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class Wrappable : MonoBehaviour {
+	private GameObject camera;
+	
 	public bool canWrap;
 	
-	public const float widthBorder  = 10.0f;
-	public const float heightBorder = 10.0f;
+	public const float widthBorder  = 100.0f;
+	public const float heightBorder = 100.0f;
 
 	// Use this for initialization
 	void Start () {
-		canWrap = false;
+		canWrap = true;
+		GameObject.FindGameObjectWithTag("Main Camera");
 	}
 	
 	// Update is called once per frame
@@ -17,21 +20,21 @@ public class Wrappable : MonoBehaviour {
 		float x = transform.position.x;
 		float y = transform.position.y;
 		
-		if (x > Screen.width + widthBorder) {
+		if (x > camera.transform.x + widthBorder) {
 			if (!canWrap) Destroy(gameObject);
 			else x = -widthBorder;
 		}
 		else if (x < -widthBorder) {
 			if (!canWrap) Destroy(gameObject);
-			else x = Screen.width + widthBorder;
+			else x = camera.transform.x + widthBorder;
 		}
-		if (y > Screen.height + heightBorder) {
+		if (y > camera.transform.y + heightBorder) {
 			if (!canWrap) Destroy(gameObject);
 			else y = -heightBorder;
 		}
 		else if (y < -heightBorder) {
 			if (!canWrap) Destroy(gameObject);
-			else y = Screen.height + heightBorder;
+			else y = camera.transform.y + heightBorder;
 		}
 		transform.position = new Vector3(x,y,0f);
 	}
