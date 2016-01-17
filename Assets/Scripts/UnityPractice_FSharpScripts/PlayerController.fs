445d3b497713cd2b83ca06f482f16686326d89f4
GerardBoberg <gerard.n.boberg@gmail.com>
2016-01-17 00:17:44 -0800

    Created Weapon Controller
    Created and integrated weapon controller.
    
    F# is a functional language. It's being used in a object-oriented game
    engine. I think there's several F# experts crying right now about how
    I'm using it.

--- Assets/Scripts/UnityPractice_FSharpScripts/PlayerController.fs
+++ Assets/Scripts/UnityPractice_FSharpScripts/PlayerController.fs
@@ -1,37 +1,42 @@
 ﻿namespace UnityPractice_FSharpScripts
 open UnityEngine
 
+
 /// Script that moves the player around and fires the weapon.
 type PlayerController() = 
     inherit MonoBehaviour()
 
     [<SerializeField>]
-    let mutable turn_rate = 250.0f // how fast it turns
+    let mutable turnRate = 250.0f // how fast it turns
     [<SerializeField>]
-    let mutable thrust_rate = 2.0f // how fast it accelerates
+    let mutable thrustRate = 2.0f // how fast it accelerates
 
     [<SerializeField>]
     [<Range(0.0f,1.0f)>] // creates a slider in the editor
-    let mutable stop_rate = 0.10f // lerp amount when decelerating
+    let mutable stopRate = 0.10f // lerp amount when decelerating
     [<SerializeField>]
-    let mutable max_speed = 10.0f // 
+    let mutable maxSpeed = 10.0f // clamps velocity.magnitude to this value
 
     let mutable velocity = Vector3.zero
 
-    //member this.Start() = 
-      //  this.transform.
+    [<DefaultValue>]
+    val mutable weaponController : UnityPractice_FSharpScripts.WeaponController
+
+    /// Retreive the weapon Controller component
+    member this.Start() = 
+        this.weaponController <- this.GetComponent<UnityPractice_FSharpScripts.WeaponController>()
     /// Handles the weapons
     member this.Update() = 
-        if Input.GetButtonDown( "Fire1" ) then
-            Debug.Log( "Fire1" )
+        if Input.GetButtonDown( "Fire1" ) then 
+            this.weaponController.Fire() |> ignore
         if Input.GetButtonDown( "Fire2" ) then
-            Debug.Log( "Fire2" )
+            this.weaponController.CycleUp() |> ignore
      
     /// Handles the motion of the player controller.
     member this.FixedUpdate() =
         // get the input, and scale 
-        let turn = Input.GetAxis( "Horizontal" ) * turn_rate * Time.fixedDeltaTime
-        let thrust = Input.GetAxis( "Vertical" ) * thrust_rate * Time.fixedDeltaTime
+        let turn = Input.GetAxis( "Horizontal" ) * turnRate * Time.fixedDeltaTime
+        let thrust = Input.GetAxis( "Vertical" ) * thrustRate * Time.fixedDeltaTime
 
         // rotates the object around the z axis
         Vector3( 0.0f, 0.0f, turn ) 
@@ -43,9 +48,9 @@ type PlayerController() =
         if thrust >= 0.0f then
             velocity <- velocity + thrust * this.transform.up
         else
-            velocity <- Vector3.Lerp( velocity, Vector3.zero, stop_rate )
+            velocity <- Vector3.Lerp( velocity, Vector3.zero, stopRate )
 
-        if( velocity.magnitude > max_speed**2.0f ) then
-            velocity <- velocity.normalized * max_speed
+        if( velocity.magnitude > maxSpeed**2.0f ) then
+            velocity <- velocity.normalized * maxSpeed
 
         this.transform.Translate( velocity * Time.fixedDeltaTime, Space.World )
---
+++ Assets/Scripts/UnityPractice_FSharpScripts/WeaponController.fs
@@ -0,0 +1,28 @@
+﻿namespace UnityPractice_FSharpScripts
+open UnityEngine
+
+/// Script that handles the weapon selection and firing
+// player input has been encapsulated away. Needs to be attached to a player Controller
+type WeaponController() = 
+    inherit MonoBehaviour()
+
+    [<SerializeField>][<DefaultValue>]
+    val mutable bulletList : GameObject[]
+
+    [<SerializeField>]
+    let mutable currentWeapon = 0
+
+    [<SerializeField>]
+    let mutable weaponOffset = 1.0f
+
+    member this.CycleUp() = 
+        currentWeapon <- ( currentWeapon + 1 ) % this.bulletList.Length
+
+    member this.CycleDown() = 
+        currentWeapon <- ( currentWeapon - 1 ) % this.bulletList.Length
+
+    member this.Fire() =
+        let wo = this.bulletList.[currentWeapon]
+        let po = this.transform.position + ( this.transform.up * weaponOffset )
+        let ro = this.transform.rotation
+        GameObject.Instantiate( wo, po , ro )
